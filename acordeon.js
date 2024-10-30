018cb167f670a76d6d235f98fc462b4d4c5c4b81
Lucas Econo <lucas.romeo18@gmail.com>
2024-10-30 09:04:37 -0300

    first commit

---
+++ acordeon.js
@@ -0,0 +1,17 @@
+let buttonClick = document.getElementById(`button`);
+let paraclick = document.getElementById(`para`);
+
+
+buttonClick.addEventListener('click', function(){
+    if(paraclick.style.display === `block`){
+        paraclick.style.display = 'none';
+    }
+    else{
+        paraclick.style.display = 'block';
+    }
+    
+})  
+
+
+
+
