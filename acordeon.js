5595b51f821c89764e8955b8c901777eb522af8c
Lucas Econo <lucas.romeo18@gmail.com>
2024-10-30 12:15:30 -0300

    Solucionado el cambio de clases en css

--- acordeon.js
+++ acordeon.js
@@ -3,11 +3,13 @@ let paraclick = document.getElementById(`para`);
 
 
 buttonClick.addEventListener('click', function(){
-    if(paraclick.style.display === `block`){
-        paraclick.style.display = 'none';
+    if(paraclick.classList.contains(`open`)){
+        //paraclick.style.display = 'none';
+        paraclick.classList.remove(`open`);
     }
     else{
-        paraclick.style.display = 'block';
+        //paraclick.style.display = 'block';
+        paraclick.classList.add(`open`);
     }
     
 })  
